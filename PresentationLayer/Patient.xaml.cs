307df05d1a78d70b03c09e3f41a2840279fa682a
grnik <gr_nik@mail.ru>
2020-08-03 01:24:27 +0300

    Пациент создание+редактирование

---
+++ PresentationLayer/Patient.xaml.cs
@@ -0,0 +1,79 @@
+﻿using BusinessLayer;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PresentationLayer
+{
+    /// <summary>
+    /// Логика взаимодействия для Patient.xaml
+    /// </summary>
+    public partial class Patient : Window
+    {
+        public Patient()
+        {
+            InitializeComponent();
+        }
+
+        private void btAdd_Click(object sender, RoutedEventArgs e)
+        {
+            PatientBL patient;
+            try
+            {
+                patient = Get();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Не верно указаны данные.");
+                return;
+            }
+            try
+            {
+                patient.Save();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Не удалось сохранить данные.");
+                return;
+            }
+            Close();
+        }
+
+        private void btCancel_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private PatientBL Get()
+        {
+            PatientBL patient = new PatientBL()
+            {
+                Id = string.IsNullOrEmpty(txtId.Text) ? Guid.Empty : new Guid(txtId.Text),
+                FIO = txtFIO.Text,
+                Gender = (Gender)cbSex.SelectedIndex,
+                Birth = Convert.ToDateTime(txtBirth.Text),
+                Address = txtAddress.Text,
+                Phone = txtPhone.Text
+            };
+            return patient;
+        }
+
+        public void Set(PatientBL patient)
+        {
+            txtId.Text = patient.Id.ToString();
+            txtFIO.Text = patient.FIO;
+            cbSex.SelectedIndex = (int)patient.Gender;
+            txtBirth.Text = patient.Birth.ToString("dd/MM/yyyy");
+            txtAddress.Text = patient.Address;
+            txtPhone.Text = patient.Phone;
+        }
+    }
+}
---
+++ PresentationLayer/Patients.xaml.cs
@@ -0,0 +1,44 @@
+﻿using BusinessLayer;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PresentationLayer
+{
+    /// <summary>
+    /// Логика взаимодействия для Patients.xaml
+    /// </summary>
+    public partial class Patients : Window
+    {
+        public Patients()
+        {
+            InitializeComponent();
+        }
+
+        private void btAdd_Click(object sender, RoutedEventArgs e)
+        {
+            Patient patient = new Patient();
+            patient.ShowDialog();
+        }
+
+        private void Window_Activated(object sender, EventArgs e)
+        {
+            lvPatients.ItemsSource = PatientBL.GetAll();
+        }
+
+        private void btEdit_Click(object sender, RoutedEventArgs e)
+        {
+            Patient patient = new Patient();
+            patient.Set((PatientBL)lvPatients.SelectedItem);
+            patient.ShowDialog();
+        }
+    }
+}
